9300345c4b08d59e7c856a6219eae83fff406d91
Priyanand Shukla <priyanand.shukla@nagarro.com>
2025-10-29 15:35:52 +0530

    change build changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/",
+  //base: "./", //For local testing "enable this and run with npx serve dist"
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
